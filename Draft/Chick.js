506d72e0133099f4cecf53320229dc9e35090eaf
Ezhu156 <ez682@nyu.edu>
2016-12-12 23:42:45 -0500

    Update Chick.js

--- Draft/Chick.js
+++ Draft/Chick.js
@@ -1,51 +1,25 @@
 function Chick() {
-  var x = -5; //x-value for egg
-  var y = 800; //y-value for egg
-  var j = 500; //x-value for chick
-  var k = 810; //y-value for chick
-  var p = 0;
+  
+  this.show = function() {
+      if (count < 570) image(family, 325, 735); //the family in the intro
+      if (count >540 && count <570) { //expression of the Family after chick taken away
+        noStroke();
+        fill(255);//color of the cloud
+        ellipse(525, 725, 20, 20); //small circle
+        image(cloud1, 530, 650, cloud1.width/2,cloud1.height/1.5); //thought cloud
+        fill(0); //color for exclamation point
+        quad(560,662,565,695,575,695,580,662); //top of exclamation point
+        ellipse(571,708,15,15);//bottom of exclamation point
+      }
+    } //end show
+    //thought bubble
+  this.thought = function() {
+      noStroke();
+      fill(255);
+      if (920 <= count && count <= 1000) ellipse(560, 790, 20, 20); //small circle
+      if (930 <= count && count <= 1000) ellipse(585, 755, 40, 40); //big circle
+      if (940 <= count && count <= 1000) image(cloud1, 600, 640); //thought cloud
+      if (945 <=count && count <= 1000) image(family, 625, 660, family.width/2, family.height/2); //the family in the intro
+    } //end thought
 
-  if (count < 500) { //HOW TO MAKE THE OBJECT ROTATE AROUND ITSELF 
-    image(egg, count, 800);
-    /*
-    push();
-    translate(width / 2, height / 2);
-    rotate(count * TWO_PI / 360);
-    translate(-egg.width / 2, -egg.height / 2);
-    image(egg, count, y);
-    pop();
-
-  push();
-  rotate(2*PI);
-  translate(egg.width/2,egg.heihgt/2);
-  image(egg, count, 800);
-  pop();
-  */
-  }
-
-  if (500 <= count && count <= 525) image(c1, 500, y);
-  if (525 <= count && count <= 550) image(c2, 500, y);
-  if (550 <= count && count <= 575) image(c3, 500, y);
-  if (575 <= count && count <= 600) image(c4, 500, y);
-  if (600 <= count && count <= 625) image(c5, 500, y);
-  if (625 <= count && count <= 650) image(c6, 500, y);
-  if (650 <= count && count <= 675) image(c7, 500, y);
-  if (675 <= count && count <= 700) image(c8, 500, y);
-  if (700 <= count && count <= 800) image(c9, 500, y);
-
-  //fill(0, 100);
-  //stroke(255);
-  //rect(500, 800, 61, 78);
-  if (count >= 800 && start === false) {
-    //if (value == 1) {
-    image(hatched, j, k);
-    image(eggbottom, 500, 830);
-    //  } else {
-    //  if (700 <= count) image(c9, 500, y);
-    //  } //end value
-  } //end count
 } //end function
-
-//draw egg and basically make it stop motion of it cracking and the chick appearing
-//animation of chick being surprised and sad that family is gone
-//make game controlled by arrow keys and mouse or just by the mouse
