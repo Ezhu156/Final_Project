e4791981bb6e650183edebd7b6229f445707bd19
Ezhu156 <ez682@nyu.edu>
2016-12-12 23:44:46 -0500

    Update sketch.js

--- Draft/sketch.js
+++ Draft/sketch.js
@@ -1,5 +1,4 @@
-var baby, family, egg, hatched, eggbottom;
-var c1, c2, c3, c4, c5, c6, c7, c8, c9; //egg hatching
+var family, hatched;
 var light, stairs, lighthouse, stairs_glow, lighthouse_glow; //ighthouse locations
 var barn, inside, ladder, ladder_glow, barn_glow, barn_exit; //barn locations
 var windmill, in_wind, windmill_glow; //windmill locations
@@ -17,44 +16,37 @@ var d = 0; //x location of the clouds
 var e = 100; //y location of the clouds
 var bird;
 var arrow, leftarrow; //arrow that shows which way the chick can go
-
+var fam_index;
+var ex_count = -250; //x location of eagle
+var ey_count = 150; //y location of eagle 
+var swoop; //eagle + chick
+var eagle; //eagle
+var ex = -50; //eagle + chick x location
+var spin; //spinning of the windmill
 
 var barn_out = true;
 var barn_in = false;
 var barn_up = false;
-var mill_out = false;
+var mill_out = true;
 var mill_in = false;
-var light_out = false;
+var light_out = true;
 var light_in = false;
 var light_up = false;
-var start = false;
+
 
 //STILL NEED TO WORK ON:
-//ADJUSTING THE LOCATIONS + HOW MOUSE PRESSED AFFECTS THEM- USE TRUE/FALSE
 //INCORPORATING THE FAMILY
-//FINDING WAYS TO MAKE IT SO THE CHICK KNOWS IT IS MISSING ITS FAMILY
-//MAKING THE EGG ROLL IN (ROTATION)
 //INCORPORATING SOUND
-//ADD A BACK BUTTON TO RETURN TO PREVIOUS PARTS IN CERTAIN LOCATION
-//MAKE THE WINDMILL SPIN SLOWLY
+//DRAW A EXIT BUTTON AND INSERT IT INTO HERE
+//FIX THE SPIN ON THE WINDMILL BECAUSE IT KEEPS SHIFTING OVER WHILE IT SPINS
+//FIX THE COUNT OF EVERYTHING SO IT FLOWS MORE SMOOTHLY
+//FIX THE JUMP ON THE CHICK
 
 function preload() {
-  baby = loadImage("pics/baby_chick.png"); //chick
   family = loadImage("pics/family_chicks.png"); //family of chickens
   lighthouse = loadImage("pics/lighthouse.png"); //lighthouse
   barn = loadImage("pics/barn.png"); //barn
   windmill = loadImage("pics/windmill.png"); //windmill
-  egg = loadImage("pics/egg.png"); //egg
-  c1 = loadImage("pics/c1.png");
-  c2 = loadImage("pics/c2.png");
-  c3 = loadImage("pics/c3.png");
-  c4 = loadImage("pics/c4.png");
-  c5 = loadImage("pics/c5.png");
-  c6 = loadImage("pics/c6.png");
-  c7 = loadImage("pics/c7.png");
-  c8 = loadImage("pics/c8.png");
-  c9 = loadImage("pics/c9.png");
-  eggbottom = loadImage("pics/eggbottom.png"); //bottom half of the egg
   hatched = loadImage("pics/hatching_chick.png"); //chick out of the shell
   light = loadImage("pics/light.png"); //top of the lighthouse
   stairs = loadImage("pics/stairs.png"); //stairs in the lighthouse
@@ -71,26 +63,40 @@ function preload() {
   stairs_glow = loadImage("pics/stairs_glow.png");
   lighthouse_glow = loadImage("pics/lighthouse_glow.png");
   barn_exit = loadImage("pics/barn_exit.png");
+  eagle = loadAnimation("pics/2.png", "pics/19.png");
+  swoop = loadAnimation("pics/e2.png", "pics/e19.png");
+  spin=loadAnimation("pics/w1.png","pics/w8.png");
 } //end preload
+
 function setup() {
   createCanvas(1000, 900);
   bird = new Bird();
   mill = new Windmill();
   house = new Lighthouse();
   farm = new Barn();
-
+  chick = new Chick();
+  eagleIntro = new Eagle();
+  fam_index = random(0, 3);
+  sky = new Sky();
+  console.log(fam_index);
 
 } //end setup
 
 function draw() {
 
+  //sky function
+  sky.sky();
+  sky.sun();
+  sky.ground();
+  sky.clouds();
+  fill(0, 100);
+  rect(885, 15, 100, 50);
   //start of locations
-
-  Sky();
-  if (n == a) image(arrow, 850, 720), image(left_arrow, 50, 720);
-
+  // if (count > 1000) {
+  // if (n == a) image(arrow, 850, 720), image(left_arrow, 50, 720);
+  //  }
   //barn
-  if ((count > 850) && (start === true)) {
+  if ((count >= 1000)) {
     if (keyIsDown(LEFT_ARROW)) { //movement of the chick to the left
       n -= 5;
       a -= 5;
@@ -117,8 +123,8 @@ function draw() {
     } //end barn
 
     //lighthouse
-    else if (a < 0 - (baby.width)) { //outside the lighthouse
-      house.outside();
+    else if (a < 0 - (hatched.width)) { //outside the lighthouse
+      if (light_out === true) house.outside();
       if ((mouseX >= 100 && mouseX < 292) && (mouseY >= 82 && mouseY < 513)) image(lighthouse_glow, 84, 67);
       n = width + a;
       if (light_in === true) { //stairs in the lighthouse
@@ -134,8 +140,10 @@ function draw() {
       n = a - (2 * width);
       if (mill_in === true) { //inside the windmill
         mill.inside();
-      } else { //outside the windmill
+      }
+      if (mill_out === true) { //outside the windmill
         mill.outside();
+
         if ((mouseX >= 698 && mouseX < 752) && (mouseY >= 728 && mouseY < 800)) image(windmill_glow, 683, 713);
       } //end else
     } //end windmill
@@ -147,18 +155,15 @@ function draw() {
       image(left_arrow, 50, 720);
       n = a;
     } //end start location
-
-    //image(hatched, n, c); //image of the chick
-    bird.update();
-    bird.show();
-
-    // image(family, 500, 700);
+    if (count > 1000) image(hatched, n, c); //chick that moves after intro
   } //end count
-
-
-  Chick(); //how the chick moves in and hatches
-
-  //fill(0);
+  if (count < 1000) eagleIntro.show();
+  if (count >= 1000) {
+    bird.show();
+    bird.update();
+  }
+  chick.thought(); //thoughtbubble of chick missing family
+  chick.show(); //how the chick moves in and hatches
   count += 1;
   //textSize(50);
   //text("count: " + count, 5, 100);
@@ -167,28 +172,39 @@ function draw() {
 function mousePressed() { //affects the locations
   if ((mouseX >= 100 && mouseX < 292) && (mouseY >= 82 && mouseY < 513)) {
     light_in = true;
+    light_out = false;
+    light_up = false;
   }
   if ((mouseX >= 0 && mouseX < 307) && (mouseY >= 513 && mouseY < 810)) {
     light_up = true;
     light_in = false;
+    light_out = false
   } else if ((mouseX >= 184 && mouseX < 446) && (mouseY >= 572 && mouseY < 801)) {
     barn_in = true;
     barn_out = false;
- /* } else if ((mouseX >= 100 && mouseX < 292) && (mouseY >= 82 && mouseY < 513)) {
-    barn_in = false;
-    barn_out = true;*/
+    barn_up = false;
   } else if ((mouseX >= 814 && mouseX < 915) && (mouseY >= 145 && mouseY < 900) && barn_in === true) {
     barn_up = true;
     barn_in = false;
     barn_out = false;
   } else if ((mouseX >= 698 && mouseX < 752) && (mouseY >= 728 && mouseY < 800)) {
     mill_in = true;
+    mill_out = false;
+  } else if ((mouseX >= 885 && mouseX < 985) && (mouseY >= 15 && mouseY < 65)) {
+    barn_out = true;
+    barn_in = false;
+    barn_up = false;
+    mill_in = false;
+    mill_out = true;
+    light_out = true;
+    light_in = false;
+    light_up = false;
+
   } //end if
 } //end mousePressed
 
 function keyPressed() {
   if (keyCode == 38) {
     bird.up();
-    start = true;
-  }
-}
+  } //end keyCode
+} //end keyPressed
