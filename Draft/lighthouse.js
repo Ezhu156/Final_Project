5cea208ede5b44762e2b35400672956fce134e2d
Ezhu156 <ez682@nyu.edu>
2016-12-14 22:53:13 -0500

    Update lighthouse.js

--- Draft/lighthouse.js
+++ Draft/lighthouse.js
@@ -1,18 +1,22 @@
 function Lighthouse() {
-  this.outside = function() {
+  this.outside = function() {//outside of the lighthouse
       fill(255, 225, 170);
       rect(0, 800, width, 100);
       image(lighthouse, 100, 82);
       image(arrow, 850, 720);
-
     } //end outside
 
-  this.inside = function() {
+  this.inside = function() {//inside of the lighthouse
       background(255);
       image(stairs, 0, 0);
+      image(outside, 885, 15);
+      if (fam_index0 === true) image(family, 300, 735);
     } //end inside
-  this.upstairs = function() {
+    
+  this.upstairs = function() {//upstairs of the lighthouse
       background(0, g, b);
       image(light, 0, 0);
+      image(outside, 885, 15);
+      if (fam_index1 === true) image(family, 165, 750);
     } //end inside
 } //end function
