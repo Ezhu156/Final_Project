071610dbef650d1008e0f8dea1e0ad29b6cfb937
Ezhu156 <ez682@nyu.edu>
2016-12-18 20:05:29 -0500

    Update Sky.js

--- Draft/Sky.js
+++ Draft/Sky.js
@@ -2,17 +2,17 @@ function Sky() {
   this.sky = function() {
       //sky color
       background(0, g, b); //sky
-      if (count < 10000) b += .05, g += .025; //sunrise
-      else b -= .05, g -= .025; //sunset
+      if (count < 4000) b += .1, g += .05; //sunrise
+      else b -= .12, g -= .07; //sunset
       if (b<50) b=50, g=25;
     } //end sky
 
   this.sun = function() {
       //location of the sun
-      if (count <= 8000) s -= .1; //sun rise
-      if (count > 8000 && count <= 9500) s -= 0.1, t -= 0.2; //sun moving towards center
-      if (count > 9500 && count <= 12000) s += 0.1, t -= 0.2; //sun moving towards left
-      if (count > 12000 && count <= 19000) s += 0.1; //sunset
+      if (count <= 3000) s -= .2; //sun rise
+      if (count > 3000 && count <= 4000) s -= 0.2, t -= 0.3; //sun moving towards center
+      if (count > 4000 && count <= 5500) s += 0.2, t -= 0.3; //sun moving towards left
+      if (count > 5500 && count <= 8000) s += 0.2; //sunset
 
       //sun
       noStroke();
@@ -29,8 +29,8 @@ function Sky() {
 
   this.clouds = function() {
       //clouds
-      if (d > width) d = 0, e = random(300);
-      else d += .1, e = e;
+      if (d-200 > width) d = 0, e = random(300);
+      else d += .5, e = e;
       image(cloud1, d, e);
       image(cloud2, d - 200, e + 200);
     } //end cloud
